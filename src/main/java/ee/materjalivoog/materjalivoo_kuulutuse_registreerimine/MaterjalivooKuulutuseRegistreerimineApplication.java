fe3f6a05a7db45bfb139b437ebd59ca2753d5a5b
Õpilane <opilane@ak.sise>
2021-12-06 09:47:15 +0200

    Initial commit

---
+++ src/main/java/ee/materjalivoog/materjalivoo_kuulutuse_registreerimine/MaterjalivooKuulutuseRegistreerimineApplication.java
@@ -0,0 +1,13 @@
+package ee.materjalivoog.materjalivoo_kuulutuse_registreerimine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterjalivooKuulutuseRegistreerimineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterjalivooKuulutuseRegistreerimineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/materjalivoog/materjalivoo_kuulutuse_registreerimine/MaterjalivooKuulutuseRegistreerimineApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.materjalivoog.materjalivoo_kuulutuse_registreerimine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterjalivooKuulutuseRegistreerimineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
