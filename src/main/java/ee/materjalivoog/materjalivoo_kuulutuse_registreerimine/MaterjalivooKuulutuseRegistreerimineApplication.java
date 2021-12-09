f294b17802934ba545c66c7c7fa17e03f0b028de
SimonF <feldingsimon@gmail.com>
2021-12-09 10:49:08 +0200

    Database changelog 1.0 commit

--- src/main/java/ee/materjalivoog/materjalivoo_kuulutuse_registreerimine/MaterjalivooKuulutuseRegistreerimineApplication.java
+++ src/main/java/ee/materjalivoog/materjalivoo_kuulutuse_registreerimine/MaterjalivooKuulutuseRegistreerimineApplication.java
@@ -3,6 +3,8 @@ package ee.materjalivoog.materjalivoo_kuulutuse_registreerimine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class MaterjalivooKuulutuseRegistreerimineApplication {
 
