799895aeb45edca4b661fb0031406a93687684fb
SimonF <feldingsimon@gmail.com>
2021-12-16 11:07:03 +0200

    1.3 - lõin teenuse, mis uuendab olemasoleva kuulutuse kõiki andmevälju

--- src/main/java/ee/materjalivoog/materjalivoo_kuulutuse_registreerimine/service/ListingService.java
+++ src/main/java/ee/materjalivoog/materjalivoo_kuulutuse_registreerimine/service/ListingService.java
@@ -5,6 +5,8 @@ import ee.materjalivoog.materjalivoo_kuulutuse_registreerimine.Listing;
 import ee.materjalivoog.materjalivoo_kuulutuse_registreerimine.repository.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -21,21 +23,37 @@ public class ListingService {
         return "Listing" + listing.getListingId() + "has been added to listing database!";
     }
 
+    public Listing updateListing(Listing listing, int listingId) {
+        listingRepository.updateListing(listingId, listing.getUserId(), listing.getCategory(), listing.getSubcategory(),
+                listing.isDeadStock(), listing.getTitle(), listing.getDescription1(), listing.getUnitPrice(), listing.getUnitType(),
+                listing.getInventory(), listing.isInStock(), listing.getProfit(), listing.getDescription2(), listing.getDiscountPrice(),
+                listing.getDiscountPercentage(), listing.getRegion(), listing.getLocation(), listing.getRestriction(), listing.getTransport());
+        return listing;
+    }
+
     public List getCategories() {
         return listingRepository.getCategories();
     }
+
     public List getSubcategories() {
         return listingRepository.getSubcategories();
     }
+
     public List selectSubcategories(Integer category_id) {
         List result = listingRepository.selectSubcategories(category_id);
         return result;
     }
-  public String addCategories(Listing listing) {
-      listingRepository.createListing(listing.getListingId(), listing.getUserId(), listing.getCategory(), listing.getSubcategory(),
-               listing.isDeadStock(), listing.getTitle(), listing.getDescription1(), listing.getUnitPrice(), listing.getUnitType(),
-               listing.getInventory(), listing.isInStock(), listing.getProfit(), listing.getDescription2(), listing.getDiscountPrice(),
-               listing.getDiscountPercentage(), listing.getRegion(), listing.getLocation(), listing.getRestriction(), listing.getTransport());
+
+    public String addCategories(Listing listing) {
+        listingRepository.createListing(listing.getListingId(), listing.getUserId(), listing.getCategory(), listing.getSubcategory(),
+                listing.isDeadStock(), listing.getTitle(), listing.getDescription1(), listing.getUnitPrice(), listing.getUnitType(),
+                listing.getInventory(), listing.isInStock(), listing.getProfit(), listing.getDescription2(), listing.getDiscountPrice(),
+                listing.getDiscountPercentage(), listing.getRegion(), listing.getLocation(), listing.getRestriction(), listing.getTransport());
         return "Selected categories " + listing.getCategory() + " & " + listing.getSubcategory() + " have been saved to listing database!";
     }
+
+//    public String addDeadStock(Listing listing) {
+//        listingRepository.updateListingPg1(listing.getListingId(), listing.isDeadStock());
+//        return "Selected deadStock has been added!";
+//    }
 }
